c80a053cfc973fa3c165a810d3b4ce107a2ccad7
daniel M <daniel.marin.depablo@gmail.com>
2021-06-18 08:52:54 +0200

    install angular/cli - service - components

---
+++ SoameeFront/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthorComponent } from './components/author/author.component';
+import { BooksComponent } from './components/books/books.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthorComponent,
+    BooksComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
